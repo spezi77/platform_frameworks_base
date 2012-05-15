e27a3dfd1accf4d4aa9265ce97a0cc0d47d7e913
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2012-05-15 10:53:37 +0200

    Fix system crash introduced by themeable framework
    After entering the PIN the system crashed due to trying to unregister a
    not registered BroadcastReceiver.
    
    Handported from http://review.cyanogenmod.com/#/c/16010/
    
    Change-Id: Iad1309089208e3a615289dbc23b0f4ef9bbce4e7

--- policy/src/com/android/internal/policy/impl/SimUnlockScreen.java
+++ policy/src/com/android/internal/policy/impl/SimUnlockScreen.java
@@ -76,7 +76,7 @@ public class SimUnlockScreen extends LinearLayout implements KeyguardScreen, Vie
         @Override
         public void onReceive(Context context, Intent intent) {
             mUiContext = null;
-            mSimUnlockProgressDialog = null;
+            context.unregisterReceiver(this);
         }
     };
 
@@ -209,9 +209,17 @@ public class SimUnlockScreen extends LinearLayout implements KeyguardScreen, Vie
         }
 
         if (mSimUnlockProgressDialog == null) {
+            final Context uiContext;
+
             mUiContext = ThemeUtils.createUiContext(mContext);
 
-            final Context uiContext = mUiContext != null ? mUiContext : mContext;
+            if (mUiContext != null) {
+                ThemeUtils.registerThemeChangeReceiver(mContext, mThemeChangeReceiver);
+                uiContext = mUiContext;
+            }
+            else {
+                uiContext = mContext;
+            }
 
             mSimUnlockProgressDialog = new ProgressDialog(uiContext);
             mSimUnlockProgressDialog.setMessage(
