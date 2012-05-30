210058ea250cf7082865fd9b75004d7cbbeb8dfd
Arham Jamal <arhamjamal@gmail.com>
2012-05-30 23:11:06 +0530

    Revert "Quad lockscreen with sms and phone"
    This reverts commit 65ec5c79dcd73b94059e938e85da5de4111c39eb.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
@@ -190,18 +190,14 @@ public class KeyButtonView extends ImageView {
                         ObjectAnimator.ofFloat(this, "glowAlpha", 1f),
                         ObjectAnimator.ofFloat(this, "glowScale", GLOW_MAX_SCALE_FACTOR)
                     );
-
-                    as.setDuration(10);
-
+                    as.setDuration(20);
                 } else {
                     as.playTogether(
                         ObjectAnimator.ofFloat(this, "glowAlpha", 0f),
                         ObjectAnimator.ofFloat(this, "glowScale", 1f),
                         ObjectAnimator.ofFloat(this, "drawingAlpha", BUTTON_QUIESCENT_ALPHA)
                     );
-
-                    as.setDuration(100);
-
+                    as.setDuration(200);
                 }
                 as.start();
             }
