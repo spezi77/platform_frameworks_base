e6f1dca0e2aff8890a0aa6c8baa9feeee0436cd6
DvTonder <david.vantonder@gmail.com>
2012-11-23 22:18:40 +0700

    Framework: Port CM9 features to CM10 This commit includes:  - Profiles
    Change-Id: Ibd63116df90b06f6ce6adb8a0343059bbb999bfb
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	Android.mk
    	core/java/android/app/ContextImpl.java
    	core/res/res/values/strings.xml
    	core/res/res/values/symbols.xml
    	policy/src/com/android/internal/policy/impl/GlobalActions.java
    
    Conflicts:
    	core/java/android/app/ContextImpl.java
    	core/res/res/values/strings.xml
    	core/res/res/values/symbols.xml
    	policy/src/com/android/internal/policy/impl/GlobalActions.java
    
    Conflicts:
    	core/java/android/app/ContextImpl.java
    	core/java/android/provider/Settings.java
    	core/res/res/values/strings.xml
    	core/res/res/values/symbols.xml
    	policy/src/com/android/internal/policy/impl/GlobalActions.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4177,6 +4177,98 @@ public final class Settings {
          */
         public static final String POWER_MENU_SILENT_ENABLED = "power_menu_silent_enabled";
 
+        /**
+         * Show the pending notification counts as overlays on the status bar
+         * @hide
+         */
+        public static final String SYSTEM_PROFILES_ENABLED = "system_profiles_enabled";
+
+        /**	
+         * Allows to show the background activity back the lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_SEE_THROUGH = "lockscreen_see_through";
+
+        /**
+         * Whether to enable custom rebindings of the actions performed on
+         * certain key press events.
+         * @hide
+         */
+        public static final String HARDWARE_KEY_REBINDING = "hardware_key_rebinding";
+
+         /**
+          * Action to perform when the home key is long-pressed. (Default is 2)
+          * 0 - Nothing
+          * 1 - Menu
+          * 2 - App-switch
+          * 3 - Search
+          * 4 - Voice search
+          * 5 - In-app search
+          * @hide
+          */
+         public static final String KEY_HOME_LONG_PRESS_ACTION = "key_home_long_press_action";
+
+         /**
+          * Action to perform when the menu key is pressed. (Default is 1)
+          * (See KEY_HOME_LONG_PRESS_ACTION for valid values)
+          * @hide
+          */
+         public static final String KEY_MENU_ACTION = "key_menu_action";
+
+         /**
+          * Action to perform when the menu key is long-pressed.
+          * (Default is 0 on devices with a search key, 3 on devices without)
+          * (See KEY_HOME_LONG_PRESS_ACTION for valid values)
+          * @hide
+          */
+         public static final String KEY_MENU_LONG_PRESS_ACTION = "key_menu_long_press_action";
+
+         /**
+          * Action to perform when the assistant (search) key is pressed. (Default is 3)
+          * (See KEY_HOME_LONG_PRESS_ACTION for valid values)
+          * @hide
+          */
+         public static final String KEY_ASSIST_ACTION = "key_assist_action";
+
+         /**
+          * Action to perform when the assistant (search) key is long-pressed. (Default is 4)
+          * (See KEY_HOME_LONG_PRESS_ACTION for valid values)
+          * @hide
+          */
+         public static final String KEY_ASSIST_LONG_PRESS_ACTION = "key_assist_long_press_action";
+
+         /**
+          * Action to perform when the app switch key is pressed. (Default is 2)
+          * (See KEY_HOME_LONG_PRESS_ACTION for valid values)
+          * @hide
+          */
+         public static final String KEY_APP_SWITCH_ACTION = "key_app_switch_action";
+
+         /**
+          * Action to perform when the app switch key is long-pressed. (Default is 0)
+          * (See KEY_HOME_LONG_PRESS_ACTION for valid values)
+          * @hide
+          */
+         public static final String KEY_APP_SWITCH_LONG_PRESS_ACTION = "key_app_switch_long_press_action";
+
+        /**
+         * Whether power menu profiles switcher is enabled
+         * @hide
+         */
+        public static final String POWER_MENU_PROFILES_ENABLED = "power_menu_profiles_enabled";
+
+        /**
+         * Whether power menu airplane toggle is enabled
+         * @hide
+         */
+        public static final String POWER_MENU_AIRPLANE_ENABLED = "power_menu_airplane_enabled";
+
+        /**
+         * Whether power menu silent mode is enabled
+         * @hide
+         */
+        public static final String POWER_MENU_SILENT_ENABLED = "power_menu_silent_enabled";
+
         /**	
          * Allows to show the background activity back the lockscreen
          * @hide
