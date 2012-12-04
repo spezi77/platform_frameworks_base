fddb6c657f15d778223c1a180e55cbdabc1e57de
iceandfire <arhamjamal@gmail.com>
2012-12-04 23:40:00 +0530

    Frameworks - Make NavBar hide [1/2]
    Needs RC commit to bring back the NavBar
    controls in the power dialog.
    
    Change-Id: If998104415e20808fbd1d5ee644457c8963fd61f
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -565,6 +565,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 			resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.NAVIGATION_BAR_SHOW), false, this);
 			resolver.registerContentObserver(Settings.System.getUriFor(
+					Settings.System.NAVIGATION_BAR_SHOW_NOW), false, this);
+			resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.NAVIGATION_BAR_HEIGHT), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.NAVIGATION_BAR_HEIGHT_LANDSCAPE), false, this);
@@ -1264,6 +1266,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mUserNavBarWidth = NavWidth;
             if(mDisplay != null)
                 setInitialDisplaySize(mDisplay, mUnrestrictedScreenWidth, mUnrestrictedScreenHeight, density);
+        }
+		if (mHasNavigationBar != showNavBarNow) {
+            mHasNavigationBar = showNavBarNow;
+            if(mDisplay != null)
+                setInitialDisplaySize(mDisplay, mUnrestrictedScreenWidth, mUnrestrictedScreenHeight,
+ 			density);
         }
     }
 
