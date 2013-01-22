19902eee25723b6641e488408c756d5b84d69e5e
Jesús David <jedga_95@outlook.com>
2013-01-22 18:25:44 -0500

    PAC switch > GONE

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2878,18 +2878,11 @@ public final class Settings {
         public static final String NAVIGATION_BAR_BACKGROUND_STYLE = "navigation_bar_background_style";
 
         /**
-         * Whether to enable or not pie controls
+         * Whether to enable or not pie controls (who wouldn't?!)
          * The value is boolean (1 or 0).
          * @hide
          */
         public static final String PIE_CONTROLS = "pie_controls";
-
-        /**
-         * Whether to enable or not pie per-app-coloring
-         * The value is boolean (1 or 0).
-         * @hide
-         */
-        public static final String PIE_PAC = "pie_pac";
 
         /**
          * Location of the pie in the screen
