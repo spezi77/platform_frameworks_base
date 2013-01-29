fee81e013d871b3b186c341d98f5ac78ae003c47
Daniel <daxx4182@gmail.com>
2013-01-29 22:03:08 +0800

    Changes
    To do/Help needed:
    
    Having this force close when trying to attempt to load a Tile with collapseAllPanels() type on Tablet UI Mode, it leads me to PanelBar.java, ln 205, but i have no idea where this error leading me too.. Help is appreciated, thanks.
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/tablet/NotificationPanel.java
    
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -508,7 +508,7 @@ public class QuickSettings {
                 quick.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        mBar.collapseAllPanels();
+                        mBar.collapseAllPanels(true);
                         final UserManager um =
                                 (UserManager) mContext.getSystemService(Context.USER_SERVICE);
                         if (um.getUsers(true).size() > 1) {
@@ -633,7 +633,7 @@ public class QuickSettings {
                 quick.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        mBar.collapseAllPanels();
+                        mBar.collapseAllPanels(true);
                         showBrightnessDialog();
                     }
                 });
@@ -1292,7 +1292,7 @@ public class QuickSettings {
                     @Override
                     public void onClick(View v) {
                         try {
-                            mBar.collapseAllPanels();
+                            mBar.collapseAllPanels(true);
                             Intent intent = new Intent(Settings.ACTION_SHOW_INPUT_METHOD_PICKER);
                             PendingIntent pendingIntent = PendingIntent.getBroadcast(mContext, 0, intent, 0);
                             pendingIntent.send();
@@ -1385,7 +1385,7 @@ public class QuickSettings {
                         Settings.System.QUICK_TOGGLE_FAV_CONTACT);
 
                         if (lookupKey != null && lookupKey.length() > 0) {
-                            mBar.collapseAllPanels();
+                            mBar.collapseAllPanels(true);
                             Uri lookupUri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_LOOKUP_URI, lookupKey);
                             Uri res = ContactsContract.Contacts.lookupContact(mContext.getContentResolver(), lookupUri);
                             Intent intent = ContactsContract.QuickContact.composeQuickContactsIntent(
