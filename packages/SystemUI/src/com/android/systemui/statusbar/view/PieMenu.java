d2748147db4551997fe27430a67a9666a2029184
Daniel <daxx4182@gmail.com>
2013-02-07 06:52:52 +0800

    Last App Toggle (for PIE)
    PS 2: derp

--- packages/SystemUI/src/com/android/systemui/statusbar/view/PieMenu.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/view/PieMenu.java
@@ -221,6 +221,7 @@ public class PieMenu extends FrameLayout {
     private boolean mEnableColor;
     private boolean mUseMenuAlways;
     private boolean mUseSearch;
+    private boolean mUseLastApp;
 
     // Animations
     private int mGlowOffsetLeft = 150;
@@ -274,6 +275,7 @@ public class PieMenu extends FrameLayout {
         boolean expanded = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.EXPANDED_DESKTOP_STATE, 0) == 1;
         mUseMenuAlways = Settings.System.getInt(mContext.getContentResolver(), Settings.System.PIE_MENU, 0) == 1;
+        mUseLastApp = Settings.System.getInt(mContext.getContentResolver(), Settings.System.PIE_LAST_APP, 0) == 1;
         mUseSearch = Settings.System.getInt(mContext.getContentResolver(), Settings.System.PIE_SEARCH, 1) == 1;
         mStatusMode = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.PIE_MODE, 2);
@@ -601,7 +603,8 @@ public class PieMenu extends FrameLayout {
     }
 
     private boolean canItemDisplay(PieItem item) {
-        return !(item.getName().equals(PieControl.MENU_BUTTON) && !mPanel.currentAppUsesMenu() && !mUseMenuAlways) &&
+        return !(item.getName().equals(PieControl.LAST_APP_BUTTON) && !mUseLastApp) &&
+                !(item.getName().equals(PieControl.MENU_BUTTON) && !mPanel.currentAppUsesMenu() && !mUseMenuAlways) &&
                 !(item.getName().equals(PieControl.SEARCH_BUTTON) && !mUseSearch);
     }
 
