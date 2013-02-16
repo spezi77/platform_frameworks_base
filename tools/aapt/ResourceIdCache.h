7671c1ec72b3ae52ac2ea61ec925be6e4abb3067
iceandfire <arhamjamal@gmail.com>
2013-02-17 02:08:44 +0530

    Derp
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- tools/aapt/ResourceIdCache.h
+++ tools/aapt/ResourceIdCache.h
@@ -1,28 +1,10 @@
 //
 // Copyright 2012 The Android Open Source Project
 //
-// Cache for resIds - we tend to lookup the same thing repeatedly
-//
 // Manage a resource ID cache.
 
 #ifndef RESOURCE_ID_CACHE_H
 #define RESOURCE_ID_CACHE_H
-
-#include "StringPool.h"
-
-class ResourceIdCache {
-public:
-    static uint32_t lookup(const String16& package,
-                           const String16& type,
-                           const String16& name,
-                           bool onlyPublic);
-
-    static bool store(const String16& package,
-                      const String16& type,
-                      const String16& name,
-                      bool onlyPublic,
-                      uint32_t resId);
-};
 
 namespace android {
 class android::String16;
