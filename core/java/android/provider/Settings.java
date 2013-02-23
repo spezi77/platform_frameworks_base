d938f42583b5f1184559a0382545cc245a62f7ea
Stevespear426 <stephen.k.spear@gmail.com>
2013-02-23 17:21:46 +0100

    Spen Options 1/2: Enable Spen options
    brought up from http://gerrit.sudoservers.com/#/c/4309/
    
    Patchset 2: Add options for removing and inserting the spen
                Not working yet... Uploading to see it better.
    
    Patchset 3: Rebase off romans toggles and the new awesome
                action awesomeness.
    
    Patchset 4: Clean up and abandon insert and reomve actions for now
    
    Change-Id: Id1e0280163bdb72bd1d38d6825b144e4c599e220
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/java/com/android/internal/util/aokp/AwesomeConstants.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2718,6 +2718,27 @@ public final class Settings {
          * @hide
          */
         public static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
+        
+        /**
+         * Custom Spen Actions
+         *
+         * @hide
+         */
+        public static final String[] SPEN_ACTIONS = new String[] {
+                "spen_action_left",
+                "spen_action_right",
+                "spen_action_down",
+                "spen_action_up",
+                "spen_action_double",
+                "spen_action_long",
+        };
+
+        /**
+         * Setting to enable Spen actions.
+         *
+         * @hide
+         */
+        public static final String ENABLE_SPEN_ACTIONS = "enable_spen_actions";
 
        /**
         * Lefty mode
