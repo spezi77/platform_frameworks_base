b101874dcf4570f2c9a3852f6ac6516f29cb284f
Bajee11 <bajee11@gmail.com>
2013-03-14 02:17:32 -0400

    Missed Call Breath (2/3)
    Credits: @Kroz for the icons and idea!
    
    This will give your Missed Call notifications a breathing effect in the statusbar instead of an static icon.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3864,6 +3864,12 @@ public final class Settings {
          */
         public static final String MMS_BREATH = "mms_breath";
 
+        /**	
+         * Give MMS Notifications a breathing effect
+         * @hide
+         */
+        public static final String MISSED_CALL_BREATH = "missed_call_breath";
+
         /**
          * Battery warning preferences
          *
