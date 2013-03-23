e29c7714bd278a5f3a290fb9ca26afc90f23895d
iceandfire <arhamjamal@gmail.com>
2013-03-23 21:33:17 +0530

    Fix carrier label
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
@@ -236,6 +236,7 @@ public class NetworkController extends BroadcastReceiver {
         // broadcasts
         IntentFilter filter = new IntentFilter();
         filter.addAction("com.aokp.romcontrol.LABEL_CHANGED");
+        filter.addAction("com.android.settings.LABEL_CHANGED");
         filter.addAction(WifiManager.RSSI_CHANGED_ACTION);
         filter.addAction(WifiManager.WIFI_STATE_CHANGED_ACTION);
         filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
@@ -423,6 +424,8 @@ public class NetworkController extends BroadcastReceiver {
             refreshLocale();
             updateAirplaneMode();
             refreshViews();
+        } else if (action.equals("com.android.settings.LABEL_CHANGED")) {
+        	refreshViews();
         } else if (action.equals(WimaxManagerConstants.NET_4G_STATE_CHANGED_ACTION) ||
                 action.equals(WimaxManagerConstants.SIGNAL_LEVEL_CHANGED_ACTION) ||
                 action.equals(WimaxManagerConstants.WIMAX_NETWORK_STATE_CHANGED_ACTION)) {
@@ -1203,10 +1206,13 @@ public class NetworkController extends BroadcastReceiver {
             }
         }
 
-        if (customLabel != null && customLabel.length() > 0) {	
-            combinedLabel = customLabel;	
-            mobileLabel = customLabel;	
-            wifiLabel = customLabel;	
+        if (customLabel != null && customLabel.trim().length() > 0) {  
+            combinedLabel = customLabel;    
+            mobileLabel = customLabel;
+            if (Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.NOTIFICATION_SHOW_WIFI_SSID, 0) == 0) {
+                wifiLabel = customLabel;
+            }
         }
 
         if (DEBUG) {
