5d398c931597a61e447095e4650f68f8f18a5770
drcmda <drcmda@gmail.com>
2013-05-02 19:11:58 +0100

    pretty much the last severe bug
    Change-Id: Idc7d6fde0f682fb115c4bfba17a4916fa8413dd6

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -5084,7 +5084,7 @@ public class Activity extends ContextThemeWrapper
 
             parent = null;
 
-            mWindow = PolicyManager.makeNewWindow(context);
+            mWindow = PolicyManager.makeNewWindow(this);
             mWindow.mIsMultiWindow = true;
             mWindow.setCloseOnTouchOutsideIfNotSet(true);
             mWindow.setGravity(Gravity.CENTER);
