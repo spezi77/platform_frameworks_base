5da54b0e9f20131b358b72141a24bb6ba06cbdd4
BigBrother1984 <stevewatersy@gmail.com>
2013-06-06 22:38:24 +0200

    Derp

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1976,6 +1976,8 @@ public class PhoneStatusBar extends BaseStatusBar {
             mNotificationPanel.expand();
             if (mFlipSettingsView.getVisibility() != View.VISIBLE) {
                 flipToSettings();
+            } else {
+                switchToSettings();
             }
         } else if (mSettingsPanel != null) {
             mSettingsPanel.expand();
