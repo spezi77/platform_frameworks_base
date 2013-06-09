7bae740bbc3b06efecf80514ee1426db5f9f9026
drcmda <drcmda@gmail.com>
2013-06-09 14:12:44 +0100

    Revert "Derp"
    This reverts commit 560762804a9498dd6ae32bc3e92c12d656d692af.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1976,8 +1976,6 @@ public class PhoneStatusBar extends BaseStatusBar {
             mNotificationPanel.expand();
             if (mFlipSettingsView.getVisibility() != View.VISIBLE) {
                 flipToSettings();
-            } else {
-                switchToSettings();
             }
         } else if (mSettingsPanel != null) {
             mSettingsPanel.expand();
