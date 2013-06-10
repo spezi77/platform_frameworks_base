56de2cc101868cac0fa6a183311503e7683a2846
drcmda <drcmda@gmail.com>
2013-06-10 09:38:45 +0100

    HALO (1/2)
    HALO is Androids one and only multitasking solution.
    
    We explicitly do not go the Samsung/Cornerstone route. This solution is basically an extension of
    the regular notificationshade, which is an aggregator for notifications that can be checked out when
    time permits. HALO completes it by allowing you to respond to tasks on the spot, trying to be as
    unintrusive as possible and flexible in regards to policies (white/black lists).
    
    Features:
    
    - Apps open up in floating state, apps underneath do not pause
    - complete SystemUI & Core integration, making HALO feel like a native feature, source/SDK and UI/UX
    - HALO has white and blacklists, which for now allow fine control for apps pinging through HALO
    - multiple gestures to navigate HALO:
      - double-tap-move: move
      - tap-drag-left/right: task through notifications, finger up launches
      - tap-drag-up: dismiss notification
      - tap-drag-down: put HALO into semi-hidden state
      - swipe while HALO is hidden wakes it up
      - drag-to-X to remove HALO altogether
    - comes with as few settings as possible, everything should be self explanatory and intutive,
      users must be informed about the double-tap gestures which is the only catch.
    
    Known issues:
    
    - Integration has been finished for PhoneUI. TabletUI and PIE must be integrated still.
    - Packageinstaller must allow HALO through its overlay check or it will disable the install-app button.
    - The low level has a few remaining issues:
      - Some apps still go up fullscale (they trick us with services, circumventing the intent flagging)
      - Some 3D games still pause
      - There is one known app that refuses to end on back or touch-outside, that is WeChat
      - The activitystack pause implementation works but is makeshift
    
    To-Do:
    
    - On-screen tutorial at first start up to teach users the gestures
    - Fixing all known issues, full integration in the remaining UI's
    - Perhaps hybrid engine integration for running floating apps in a decreased size
    
    Merge branch 'multiwindow' into jellybean
    
    Conflicts:
    	core/java/android/app/Activity.java
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values-land/dimens.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	services/java/com/android/server/NotificationManagerService.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2706,7 +2706,24 @@ public final class Settings {
         */
         public static final String SYSTEMUI_NAVRING_LONG_ENABLE = "systemui_navring_long_enable";
 
+         /* HALO, should default to 0 (no, do not show)
+         * @hide
+         */
+        public static final String HALO_ACTIVE = "halo_active";
+
         /**
+         * HALO reversed?, should default to 1 (yes, reverse)
+         * @hide
+         */
+        public static final String HALO_REVERSED = "halo_reversed";
+
+        /**
+         * HALO hide?, should default to 0 (no, do not hide)
+         * @hide
+         */
+        public static final String HALO_HIDE = "halo_hide";
+
+       /**
          * Custom navring actions
          *
          * @hide
