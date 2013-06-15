1c40a8df80360900838ca4b25fa238a46debe9d9
Daniel MM <daxx4182@gmail.com>
2013-06-15 14:38:53 +0800

    HALO: Circle, Bubble, text colors
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	packages/SystemUI/res/drawable-hdpi/halo_bg.png
    	packages/SystemUI/res/drawable-nodpi/bubble_black_l.9.png
    	packages/SystemUI/res/drawable-nodpi/bubble_black_r.9.png
    	packages/SystemUI/res/drawable-xhdpi/halo_bg.png
    	packages/SystemUI/res/layout/halo_bubble.xml
    	packages/SystemUI/res/layout/halo_speech.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/halo/HaloProperties.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3868,6 +3868,12 @@ public final class Settings {
         public static final String HALO_PAUSE = "halo_pause";
 
 		/**
+         * HALO being minimal.
+         * @hide
+         */
+        public static final String HALO_STYLE = "halo_style";
+
+        /**
          * HALO speech bubble color
          * @hide
          */
@@ -3878,18 +3884,12 @@ public final class Settings {
          * @hide
          */
         public static final String HALO_BUBBLE_TEXT_COLOR = "halo_bubble_text_color";
-        
+
         /**
-         * HALO colors
+         * HALO circle bg color
          * @hide
          */
-        public static final String HALO_COLORS = "halo_colors";
-        
-        /**
-         * HALO effect color
-         * @hide
-         */
-        public static final String HALO_EFFECT_COLOR = "halo_effect_color";
+        public static final String HALO_CIRCLE_COLOR = "halo_circle_color";
 
         /**
          *
