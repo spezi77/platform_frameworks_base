6518b6354b9ec30042c37938ad10422fece04b53
iceandfire <arhamjamal@gmail.com>
2013-09-23 19:59:37 +0530

    Fix build after active display

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -506,7 +506,7 @@ public class ActiveDisplayView extends FrameLayout {
     public void updateResources() {
         ArrayList<TargetDrawable> storedDraw = new ArrayList<TargetDrawable>();
         final Resources res = getResources();
-        final int targetInset = res.getDimensionPixelSize(com.android.internal.R.dimen.lockscreen_target_inset);
+        //final int targetInset = res.getDimensionPixelSize(com.android.internal.R.dimen.lockscreen_target_inset);
         final Drawable blankActiveDrawable =
                 res.getDrawable(com.android.internal.R.drawable.ic_lockscreen_target_activated);
         final InsetDrawable activeBack = new InsetDrawable(blankActiveDrawable, 0, 0, 0, 0);
@@ -517,8 +517,8 @@ public class ActiveDisplayView extends FrameLayout {
             storedDraw.add(new TargetDrawable(res, null));
             storedDraw.add(new TargetDrawable(res, null));
             storedDraw.add(new TargetDrawable(res, null));
-            storedDraw.add(new TargetDrawable(res, getLayeredDrawable(activeBack,
-                    mNotificationDrawable, targetInset, false)));
+            //storedDraw.add(new TargetDrawable(res, getLayeredDrawable(activeBack,
+                    //mNotificationDrawable, targetInset, false)));
             storedDraw.add(new TargetDrawable(res, null));
             if (mNotification.isClearable()) {
                 storedDraw.add(new TargetDrawable(res, res.getDrawable(R.drawable.ic_ad_dismiss_notification)));
