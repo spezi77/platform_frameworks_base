00d46b6a62abb9e87ed7df9d7ad5a94ad5568aec
iceandfire <arhamjamal@gmail.com>
2013-09-26 12:30:39 +0530

    Fav contact toggle
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
@@ -90,6 +90,7 @@ public class ToggleManager {
     public static final String WIRELESS_ADB_TOGGLE = "WIRELESSADB";
     public static final String STATUSBAR_TOGGLE = "STATUSBAR";
     public static final String CUSTOM_TOGGLE = "CUSTOM";
+    public static final String FAV_CONTACT_TOGGLE = "FAVCONTACT";
     
     private int mStyle;
 
@@ -141,6 +142,7 @@ public class ToggleManager {
             toggleMap.put(WIRELESS_ADB_TOGGLE, WirelessAdbToggle.class);
             toggleMap.put(STATUSBAR_TOGGLE, StatusbarToggle.class);
             toggleMap.put(CUSTOM_TOGGLE, CustomToggle.class);
+            toggleMap.put(FAV_CONTACT_TOGGLE, FavoriteUserToggle.class);
             // toggleMap.put(BT_TETHER_TOGGLE, null);
         }
         return toggleMap;
