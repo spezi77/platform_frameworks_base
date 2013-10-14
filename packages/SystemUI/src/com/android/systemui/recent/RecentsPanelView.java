5927bc1877e2c78e5c8b55517ebe652696845832
iceandfire <arhamjamal@gmail.com>
2013-10-15 01:18:35 +0530

    Clear cache on longpress clear button in recents
    This will clear cache on longpressing the clear button in recents menu.
    Original implementation by mnm9994u.
    Modified by me to work with AOKPs clear all implementation.
    
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -75,6 +75,8 @@ import com.android.systemui.statusbar.tablet.TabletStatusBar;
 import com.android.internal.util.MemInfoReader;
 
 import java.util.ArrayList;
+import java.io.OutputStreamWriter;
+import java.lang.Runtime;
 
 public class RecentsPanelView extends FrameLayout implements OnItemClickListener, RecentsCallback,
         StatusBarPanel, Animator.AnimatorListener {
@@ -535,7 +537,26 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
                 AwesomeAction.launchAction(v.getContext(), AwesomeConstant.ACTION_ASSIST.value());
             }
         });
+        mRecentsKillAllButton.setOnLongClickListener(new OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    killAllRecentApps();
+                    try {
+                        ProcessBuilder pb = new ProcessBuilder("su", "-c", "/system/bin/sh");
+                        OutputStreamWriter osw = new OutputStreamWriter(pb.start().getOutputStream());
+                        osw.write("sync" + "\n" + "echo 3 > /proc/sys/vm/drop_caches" + "\n");
+                        osw.write("\nexit\n");
+                        osw.flush();
+                        osw.close();
+                    } catch (Exception e) {
+                        Log.d(TAG, "Failed!");
+                    }
+                    return true;
+            }
+        });
     }
+
+
 
     public void setMinSwipeAlpha(float minAlpha) {
         if (mRecentsContainer instanceof RecentsScrollView){
