2db54e7e8c568248ae35a86d2519fe4aa1bcf649
Peter Carenza <pcarenza@gmail.com>
2013-10-30 16:00:47 -0400

    Revert "--- HALO 2.0 ---"
    This reverts commit eb5f638a923b9f989feff444cb475fca3b08a8f2.
    
    Conflicts:
    	packages/SystemUI/res/values/dimens.xml
    	packages/SystemUI/res/values/strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
    	packages/SystemUI/src/com/android/systemui/statusbar/halo/HaloProperties.java
    
    Change-Id: I2d27e56b199715df3fd44a43b844a24172fc6d30

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -99,7 +99,6 @@ import android.view.ViewGroup.LayoutParams;
 import android.view.WindowManager;
 import android.view.WindowManagerGlobal;
 import android.widget.FrameLayout;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupMenu;
@@ -283,9 +282,6 @@ public abstract class BaseStatusBar extends SystemUI implements
     private RemoteViews.OnClickHandler mOnClickHandler = new RemoteViews.OnClickHandler() {
         @Override
         public boolean onClickHandler(View view, PendingIntent pendingIntent, Intent fillInIntent) {
-        
-        android.util.Log.d("PARANOID", "Notification click handler invoked for intent: " + pendingIntent);
-        
             if (DEBUG) {
                 Slog.v(TAG, "Notification click handler invoked for intent: " + pendingIntent);
             }
@@ -1370,30 +1366,9 @@ public abstract class BaseStatusBar extends SystemUI implements
         return entry.notification;
     }
 
-    public void prepareHaloNotification(NotificationData.Entry entry, StatusBarNotification notification, boolean update) {
+    private Bitmap createRoundIcon(StatusBarNotification notification) {
 
     Notification notif = notification.getNotification();
-
-    // Get the remote view
-        try {
-
-            if (!update) {
-                ViewGroup mainView = (ViewGroup)notif.contentView.apply(mContext, null, mOnClickHandler);
-
-                if (mainView instanceof FrameLayout) {
-                    entry.haloContent = mainView.getChildAt(1);
-                    mainView.removeViewAt(1);
-                } else {
-                    entry.haloContent = mainView;
-                }
-            } else {
-                notif.contentView.reapply(mContext, entry.haloContent, mOnClickHandler);
-            }
-
-        } catch (Exception e) {
-            // Non uniform content?
-            android.util.Log.d("PARANOID", "   Non uniform content?");
-        }
 
         // Construct the round icon
         final float haloSize = Settings.System.getFloat(mContext.getContentResolver(),
@@ -1434,7 +1409,7 @@ public abstract class BaseStatusBar extends SystemUI implements
                 // NameNotFoundException
             }
         }
-        entry.roundIcon = roundIcon;
+        return roundIcon;
     }
 
     protected StatusBarIconView addNotificationViews(IBinder key,
@@ -1459,8 +1434,8 @@ public abstract class BaseStatusBar extends SystemUI implements
             return null;
         }
         
-        NotificationData.Entry entry = new NotificationData.Entry(key, notification, iconView);
-        prepareHaloNotification(entry, notification, false);
+        NotificationData.Entry entry = new NotificationData.Entry(key, notification, iconView,
+                createRoundIcon(notification));
         entry.hide = entry.notification.getPackageName().equals("com.paranoid.halo");
 
         final PendingIntent contentIntent = notification.getNotification().contentIntent;
@@ -1623,8 +1598,7 @@ public abstract class BaseStatusBar extends SystemUI implements
                     oldEntry.floatingIntent = null;
                 }
                 // Update the roundIcon
-                prepareHaloNotification(oldEntry, notification, true);
-
+                oldEntry.roundIcon = createRoundIcon(notification); 
                 // Update the icon.
                 final StatusBarIcon ic = new StatusBarIcon(notification.getPackageName(),
                         notification.getUser(),
@@ -1686,8 +1660,6 @@ public abstract class BaseStatusBar extends SystemUI implements
                 mHandler.sendEmptyMessage(MSG_HIDE_INTRUDER);
             }
         }
-        // Update halo
-        if (mHalo != null) mHalo.update();
     }
 
     // Q: What kinds of notifications should show during setup?
