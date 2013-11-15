AOKP/frameworks_base

Ported: Volume adjust sound preference 1/2
Author:    StevenHarperUK <StevenHarperUK@gmail.com>

Feature : Volume adjust sound preference 1/2

Adjusting the volume makes a nice "boing" sound
Personally I find this annoying
This patch makes this a preference in
Settings | Sound
    Volume adjust sound
        Play sound when adjusting volume with hard keys

The Checkbox is defaulted to ON (1 in defaults.xml)

Look for 2/2 for the Settings, Sound Panel Change

Change-Id: Iad89a0c49288094e21824aa9c9e1bf98c29cfec3

core/java/android/provider/Settings.java
@@ -2504,6 +2504,13 @@ public final class Settings {
          */
         public static final String ENABLE_VOLUME_OPTIONS = "enable_volume_options";
 
+        /** 
+         * Volume Adjust Sounds Enable, This is the noise made when using volume hard buttons
+         * Defaults to 1 - sounds enabled
+         * @hide
+         */
+        public static final String VOLUME_ADJUST_SOUNDS_ENABLED = "volume_adjust_sounds_enabled";
+
         /**
          * Setting to Link Ringtone and Notification.
          *

spezi77/platform_frameworks_base

Signed-off-by: iceandfire <arhamjamal@gmail.com>
Conflicts:
	packages/SettingsProvider/res/values/defaults.xml
	core/java/android/provider/Settings.java

core/java/android/provider/Settings.java
@@ -5928,6 +5928,13 @@ public final class Settings {
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
         /**
+         * Setting to Link Ringtone and Notification.
+         *
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
+        /**
          * Developer setting to force RTL layout.
          * @hide
          */