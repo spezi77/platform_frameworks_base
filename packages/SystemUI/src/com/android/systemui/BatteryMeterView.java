017a9e15119de51149e37b006e0fa6e164462105
Peter Carenza <pcarenza@gmail.com>
2013-11-26 13:26:28 -0500

    more battery tweaks
    Change-Id: Id324fdd9fc75c05aed99b9c94f99cea65193e732

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -206,7 +206,7 @@ public class BatteryMeterView extends View implements DemoMode {
         mBatteryPaint.setStyle(Paint.Style.FILL_AND_STROKE);
 
         mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mTextPaint.setColor(0xB2000000);
+        mTextPaint.setColor(res.getColor(R.color.batterymeter_text_color));
         Typeface font = Typeface.create("sans-serif-condensed", Typeface.NORMAL);
         mTextPaint.setTypeface(font);
         mTextPaint.setTextAlign(Paint.Align.CENTER);
